1a5e7e9552ccbd5adc905cfc120267f9da9ccb13
AI-WebUI <ai_webui@163.com>
2016-10-08 15:12:54 +0800

    wxw
    new

---
+++ html/desktop/docs/js/iframefit.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/10/8.
+ */
+$(window.parent.document).find("#iframe_m").load(function () {
+    var main = $(window.parent.document).find("#iframe_m");
+    var thisheight = $(document).height() + 30;
+    main.height(thisheight);
+});
