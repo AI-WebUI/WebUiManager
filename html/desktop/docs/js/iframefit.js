4d91897c8c946e30ede93eb72002de7aa671810d
AI-WebUI <ai_webui@163.com>
2016-10-08 15:31:19 +0800

    3333
    333

--- html/desktop/docs/js/iframefit.js
+++ html/desktop/docs/js/iframefit.js
@@ -1,9 +1,12 @@
 /**
  * Created by Administrator on 2016/10/8.
  */
-$(window.parent.document).find("#iframe_m").load(function () {
-    var main = $(window.parent.document).find("#iframe_m");
-    //var thisheight = $(document).height() + 30;
-    //main.height(thisheight);
-   main.style.height = main.contentWindow.document.body.scrollHeight +0 + "px";
-});
+function fitheight(){
+    $(window.parent.document).find("#iframe_m").load(function () {
+        var main = $(window.parent.document).find("#iframe_m");
+        //var thisheight = $(document).height() + 30;
+        //main.height(thisheight);
+        main.style.height = main.contentWindow.document.body.scrollHeight +0 + "px";
+    });
+}
+setInterval(fitheight,500);
