7a699c13236efa6460b090ddcbbd713f8aad2049
AI-WebUI <ai_webui@163.com>
2016-10-12 14:22:38 +0800

    3452w
    wee

--- html/desktop/docs/js/totop.js
+++ html/desktop/docs/js/totop.js
@@ -6,11 +6,11 @@ var screenw =document.documentElement.clientWidth || document.body.clientWidth;
 var screenh =document.documentElement.clientHeight || document.body.clientHeight;
 oTop.style.position='absolute';
 oTop.style.left = screenw - oTop.offsetWidth-10 + "px";
-oTop.style.top = screenh/2 - oTop.offsetHeight + "px";
+oTop.style.top = screenh/2 - oTop.offsetHeight +200+ "px";
 window.onscroll = function () {
 	var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
-	oTop.style.top = screenh/2 - oTop.offsetHeight + scrolltop + "px";
-	if(oTop.style.top > screenh/2 - oTop.offsetHeight + "px"){
+	oTop.style.top = screenh/2 - oTop.offsetHeight +200+ scrolltop + "px";
+	if(oTop.style.top > screenh/2 - oTop.offsetHeight+200 + "px"){
 		$("#to_top").removeClass("to_top").addClass("can_top");
 	}else if(scrolltop==0){
 		$("#to_top").removeClass("can_top").addClass("to_top");
